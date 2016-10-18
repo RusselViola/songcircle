2a8e1871541a754273a0c56b115ed6e0a17801f7
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-17 22:12:26 -0400

    removed binding.pry from userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       @user = current_user
-      binding.pry
     else
       redirect_to :new_user_session
     end
