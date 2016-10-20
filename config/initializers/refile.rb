75f791c27b87bb3e0bf41f843f4b79b88daf211e
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-20 11:13:20 -0400

    trying to update aws for heroku

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,9 +1,10 @@
 require "refile/s3"
 
 aws = {
+  s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: "s3-us-west-2.amazonaws.com",
+  region: ENV['AWS_REGION'],
   bucket: ENV['S3_BUCKET_NAME'],
 }
 
