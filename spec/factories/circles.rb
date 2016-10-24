234e40a680c07ee0d8ac08af8cca6729908843ea
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-24 17:46:50 -0400

    got rid of the last instance of circle_owner_id

--- spec/factories/circles.rb
+++ spec/factories/circles.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :circle do
     sequence(:id) { |n| n }
     sequence(:name) { |n| "M#{n}ntle Circl#{n}" }
-    sequence(:circle_owner_id) { |n| n }
+    sequence(:user_id) { |n| n }
     user
   end
 end
