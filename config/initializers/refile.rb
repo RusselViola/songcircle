722911ed38e300a27dd6161fa136dc298223463c
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-10-25 20:26:58 -0400

    added rack-cors and changed Refil.cdn_host to https

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,11 +4,11 @@ aws = {
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   region: ENV['AWS_REGION'],
-  bucket: ENV['S3_BUCKET_NAME'],
+  bucket: ENV['S3_BUCKET_NAME']
 }
 
 if Rails.env.production?
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
-  Refile.cdn_host = "http://s3.amazonaws.com/russelsoundbucket"
+  Refile.cdn_host = "https://s3.amazonaws.com/russelsoundbucket"
 end
