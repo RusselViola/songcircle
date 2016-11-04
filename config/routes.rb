141bc21cc002fad801120ec04771b05dc903d54d
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-11-04 11:07:15 -0400

    started messing with nested routes, will continue in new branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "users#show"
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :circles, only: [:index, :new, :create]
+  end
 
   resources :songs
-  resources :circles, only: [:index, :new, :create]
 
   get "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
